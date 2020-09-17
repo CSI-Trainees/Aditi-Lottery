0cf109b752d06644f008dd94f5814ffecc0e036d
aditimishra901 <aditimishra1261@gmail.com>
2020-09-18 00:50:36 +0530

    only once the button will work

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function generator(){
+
+document.getElementById("genbut").onclick = function() {
   var number = Math.floor(Math.random()*11)+10;
   document.getElementById("lot").value=number;
+    this.disabled = true;
 }
