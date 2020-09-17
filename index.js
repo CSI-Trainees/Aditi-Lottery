b3b897143cd2031b0ae9668b8d12fecf153711b4
aditimishra901 <aditimishra1261@gmail.com>
2020-09-18 02:17:21 +0530

    First Section

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 document.getElementById("genbut").onclick = function() {
   var number = Math.floor(Math.random()*11)+10;
   document.getElementById("lot").value=number;
+
     this.disabled = true;
+
+
 }
