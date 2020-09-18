f5bd4beefec3e58993ba79ca7abec9a82271b811
aditimishra901 <aditimishra1261@gmail.com>
2020-09-19 03:32:19 +0530

    lottery number checking and display done

--- index.js
+++ index.js
@@ -4,6 +4,29 @@ document.getElementById("genbut").onclick = function() {
   document.getElementById("lot").value=number;
 
     this.disabled = true;
+  }
 
-
-}
+  document.getElementById("checkButton").onclick = function(){
+    var lotteryNumber = document.getElementById("checkinput").value;
+    if(lotteryNumber == 12)
+    {
+      alert("Winner");
+    }
+    else if(lotteryNumber == 16)
+    {
+      alert("Winner");
+    }
+    else if(lotteryNumber == 18)
+    {
+      alert("Winner");
+    }
+    else if(lotteryNumber == NULL)
+    {
+      alert("Please enter the above number");
+    }
+    else
+    {
+      alert("sorry");
+    }
+    this.disabled = true;
+  }
