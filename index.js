334a713161239fb65d37d4c2bb3babf20fc7729f
aditimishra901 <aditimishra1261@gmail.com>
2020-09-23 05:02:02 +0530

    result image added

--- index.js
+++ index.js
@@ -5,23 +5,33 @@ document.getElementById("genbut").onclick = function() {
   this.disabled = true;
   }
 
+
+
+
+
   document.getElementById("checkButton").onclick = function(){
     var lotteryNumber = document.getElementById("checkinput").value;
-    if(lotteryNumber == 12)
-    {
-      alert("Winner");
-    }
-    else if(lotteryNumber == 16)
-    {
-      alert("Winner");
-    }
-    else if(lotteryNumber == 18)
-    {
-      alert("Winner");
-    }
-    
-    {
-      alert("sorry");
-    }
+
+      if(lotteryNumber == 12 || lotteryNumber == 16 ||lotteryNumber == 18)
+      {
+        lotterypic();
+      }
+    else
+      {
+        document.getElementsById("mypic").src = 'images/'+ pic[3]+ '.jpg';
+      }
+
+
+
     this.disabled = true;
   }
+  var pic = ["images/gryffindor.jpg","images/hufflepuff.jpg","images/ravenclaw.jpg","images/slytherin.jpg"];
+  var kb;
+function lotterypic(){
+
+  var num = Math.floor(Math.random()* pic.length)
+  console.log(num);
+  var img = pic[num];
+  console.log(img);
+
+}
